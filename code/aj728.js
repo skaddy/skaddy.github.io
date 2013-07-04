e6fdc1deb456699d09a63a9333d93a6cdda0a9ae
skaddy <andreascaduto@me.com>
2013-07-04 10:37:51 +0200

    chn2

--- code/aj728.js
+++ code/aj728.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	var response = $.get("/objects/script/728.js", function() {
-		var scriptText = "<pre><code class=\"javascript\">"+response.responseText+"</code></pre>";
+	var response = $.get("/objects/script/728.js", function(data) {
+		var scriptText = "<pre><code class=\"javascript\">"+data+"</code></pre>";
 		$('#content').html(scriptText);
 		hljs.initHighlightingOnLoad();
 	});
