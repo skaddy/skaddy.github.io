b9452a831654cd5912b906b117b7957766fbc7e2
skaddy <andreascaduto@me.com>
2013-07-04 10:22:55 +0200

    interesa

--- code/aj728.js
+++ code/aj728.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var response = $.get("objects/script/728.js", function() {
+	var response = $.get("/objects/script/728.js", function() {
 		var scriptText = "<pre><code class=\"javascript\">"+response.responseText+"</code></pre>";
 		$('#content').html(scriptText);
 		hljs.initHighlightingOnLoad();
