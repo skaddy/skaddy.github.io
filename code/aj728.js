2695c2d4aed5213d1ca813bb9f7dc93ae562411c
skaddy <andreascaduto@me.com>
2013-07-04 10:42:32 +0200

    chn3

--- code/aj728.js
+++ code/aj728.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
 		$('#content').html(scriptText);
 		hljs.initHighlightingOnLoad();
 	});
+
+	$.getScript("/objects/script/728.js", function(data, textStatus, jqxhr) {
+   		console.log(data); //data returned
+   		var scriptText = "<pre><code class=\"javascript\">"+data+"</code></pre>";
+		$('#content').html(scriptText);
+		hljs.initHighlightingOnLoad();
+	});
 });
