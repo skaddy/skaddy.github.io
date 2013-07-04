2247c8024c434a7b67052d5e5acd77dfab86fdb4
skaddy <andreascaduto@me.com>
2013-07-04 11:01:27 +0200

    chn7

--- code/aj728.js
+++ code/aj728.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-	$.getScript("/objects/script/728.js", function(data) {
-		var scriptText = "<pre><code class=\"javascript\">"+data+"</code></pre>";
+	response = jQuery.get('/objects/script/728.js', function() {
+    	var scriptText = "<pre><code class=\"javascript\">"+response.responseText+"</code></pre>";
 		$('#content').html(scriptText);
 		hljs.initHighlightingOnLoad();
-	},false);
+	});
+	var scriptText = "<pre><code class=\"javascript\">"+data+"</code></pre>";
+	$('#content').html(scriptText);
+	hljs.initHighlightingOnLoad();
 });
