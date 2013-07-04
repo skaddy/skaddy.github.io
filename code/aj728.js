d760ddc32a092a7807e525b01d46959eb391a9ff
skaddy <andreascaduto@me.com>
2013-07-04 10:21:26 +0200

    intere

---
+++ code/aj728.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	var response = $.get("objects/script/728.js", function() {
+		var scriptText = "<pre><code class=\"javascript\">"+response.responseText+"</code></pre>";
+		$('#content').html(scriptText);
+		hljs.initHighlightingOnLoad();
+	});
+});
